1928e0efd644a70e38beaf72abaa38c181b383da
juanortizj <juan.ortiziz@campusucc.edu.co>
2025-11-23 02:01:13 -0500

    Refactor Cuenta_2 constructor and getters

--- Trabajo Final/Cuenta_2.java
+++ Trabajo Final/Cuenta_2.java
@@ -3,29 +3,14 @@ public class Cuenta_2 {
     private String apellido;
     private double saldo;
 
-
-    public Cuenta_2(String nombre, String apellido,double saldo, double saldoInicial) {
+    public Cuenta_2(String nombre, String apellido, double saldo) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.saldo = saldo;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-    public String getApellido() {
-        return apellido;
-    }
-    public double getSaldo() {
-        return saldo;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public double getSaldo() { return saldo; }
 }
+
